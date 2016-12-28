aaefcc174e57611d8842217bba9e4aa99d311080
Kameron Kirk <kkirk@datto.com>
2016-12-28 14:26:57 -0500

    working on escaped characters.

--- parser_test.rb
+++ parser_test.rb
@@ -16,4 +16,10 @@ class TestParser < Minitest::Test
         r = p.convert("'datto is hiring'")
         assert r.eql? "datto is hiring"
     end
+
+    def test_convert_string_with_escapse_to_string
+        p = Parser.new
+        r1 = p.convert("'\'d\' is for \'datto\''")
+        assert r1.eql? "'d' is for 'datto'"
+    end
 end
